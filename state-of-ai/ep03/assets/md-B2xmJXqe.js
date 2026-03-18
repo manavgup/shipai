438bae86c20df11becc232f3b1d06c6bbb42aee5
manavgup <manavg@gmail.com>
2026-03-18 12:12:46 -0400

    Updates

---
+++ state-of-ai/ep03/assets/md-B2xmJXqe.js
@@ -0,0 +1,6 @@
+import{b as a,o as l,w as s,g as e,v as i,x as m,C as o}from"./modules/vue-C_NJZXjI.js";import{I as p}from"./slidev/default-DYLMErxa.js";import{u,f as c}from"./slidev/context-B5w2e-LG.js";import"./index-D_S46ttg.js";import"./modules/shiki-DPd3YiBM.js";const I={__name:"ep03.md__slidev_8",setup(d){const{$clicksContext:r,$frontmatter:n}=u();return r.setup(),(f,t)=>(l(),a(p,i(m(o(c)(o(n),7))),{default:s(()=>[...t[0]||(t[0]=[e("h1",null,"The buds of Open-Source AI",-1),e("h2",null,"- Democratized AI Development",-1),e("h2",null,"- Benefits for pure RL Approach",-1),e("pre",null,[e("code",null,`  - self-directed exploration rather than mimicking labeled examples 
+  - GRPO framework removes the need for an LLM critic
+`)],-1),e("h2",null,"- Industry Impact",-1),e("h2",null,"- Future Implications",-1),e("pre",null,[e("code",null,`  - Potential to move away from data-hungry systems
+  - Lower prices
+  - Acceleration of innovation cycle (LLaMA 4 released moved up by 6 months)
+`)],-1)])]),_:1},16))}};export{I as default};
---
+++ state-of-ai/ep07/assets/slidev/default-BTnInq-9.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-Ct_AaW29.js";import{_ as r}from"../index-BAHCIu9O.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
